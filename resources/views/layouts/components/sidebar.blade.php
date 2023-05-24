5508148e1ca7057b63fd6160ee11cf81fa389d11
Sendy Joan K <69149460+sendyjoan@users.noreply.github.com>
2023-05-24 19:11:37 +0700

    Project Init

---
+++ resources/views/layouts/components/sidebar.blade.php
@@ -0,0 +1,20 @@
+<aside id="sidebar-wrapper">
+    <div class="sidebar-brand">
+      <a href="index.html">Starter Code</a>
+    </div>
+    <div class="sidebar-brand sidebar-brand-sm">
+      <a href="index.html">LSC</a>
+    </div>
+    <ul class="sidebar-menu">
+        @section('sidebar')
+            <li class="menu-header">Dashboard</li>
+            <li class=""><a href="#" class="nav-link"><i class="fas fa-fire"></i><span>Dashboard</span></a></li>
+        @show
+      </ul>
+
+      <div class="mt-4 mb-4 p-3 hide-sidebar-mini">
+        <a href="https://getstisla.com/docs" class="btn btn-primary btn-lg btn-block btn-icon-split">
+          <i class="fas fa-rocket"></i> Documentation
+        </a>
+      </div>
+  </aside>
