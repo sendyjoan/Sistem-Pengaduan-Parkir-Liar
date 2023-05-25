20d5401cad49c8955401d90b379857dfb6cf2d21
Sendy Joan K <69149460+sendyjoan@users.noreply.github.com>
2023-05-26 01:01:56 +0700

    Fundamental Aduan Done

---
+++ app/Models/Aduan.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Aduan extends Model
+{
+    use HasFactory;
+    protected $table = 'aduans';
+    protected $fillable = [
+        'judul',
+        'deskripsi',
+        'foto',
+    ];
+
+}
